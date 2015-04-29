92c0da09ea6a38910d21e1dcf97d2f36ff1298fa
Pieter Robberechts <pieter_robberechts@msn.com>
2015-04-29 15:47:27 +0200

    Fix #12: tekst op homepagina

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -24,3 +24,16 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+ 
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
+});
